545e16bfe6a36b49742e1f0e3599e3defd22d842
Jaye Foster <jaye@thefostersonline.com>
2024-03-26 19:21:50 -0400

    updated functions

--- Working.R
+++ Working.R
@@ -2,54 +2,88 @@ library(ggplot2)
 library(glmnet)
 library(MASS)
 
-
-n<-100 #p=2
-beta<-c(1,2,3)
-rho<- c(0, 0.9)
-alpha<-c(0,0.5)
-K<-1
-
-elastic_net2<-function(n, beta, rho, alpha, K){
-  num_alpha<-length(alpha)
-  num_rho<-length(rho)
-  Rho<-rep(0, num_alpha*num_rho)
-  Alpha<-rep(0, num_alpha*num_rho)
-  mse<-matrix(0, num_rho*num_alpha, K)
-  MSE_K<-cbind(Rho, Alpha, mse)
-  
-  for(k in 1:K){
-    row=1
-    for(j in 1:num_rho){
-      cov<-matrix(data = c(1, rho[j], rho[j], 1), nrow=2, ncol=2)
-      X<-mvrnorm(n, mu=c(0,0), Sigma =cov)
-      design<-cbind(rep(1,n), X )
-      Y<-design%*%beta+rnorm(n)
-      for(i in 1:num_alpha){
-        fit_i<-cv.glmnet(X, Y, family="gaussian", alpha=alpha[i])
-        fit_i_coef<-coef(fit_i, s="lambda.min")
-        print(fit_i_coef)
-        yhat<-design%*%fit_i_coef
-        if(k<=1){
-          MSE_K[row,1]<-rho[j]
-          MSE_K[row,2]<-alpha[i]
-        }
-        print(mean((Y-yhat)^2))
-        MSE_K[row,(k+2)]<-mean((Y-yhat)^2)
-        row=row+1
-      }}}
-  results<-cbind(MSE_K[,(1:2)], 0)
-  if(K==1){
-    results[,3]<-MSE_K[,3]
-  }
-  else{
-    results[,3]<-rowMeans(MSE_K[,(3:K+2)])
-  }
-  colnames(results)[3]<-"MSE"
-  
-  
-  
-  return(as.data.frame(results))
+elastic.net.est<-function(n, X, Y, alpha){
+  fit<-cv.glmnet(X, Y, family="gaussian", alpha=alpha)
+  lambda<-fit$lambda.min
+  coefs<-coef(fit, s="lambda.min")
+  print(coefs)
+  signif.pred<-length(which(coefs!=0))
+  yhat<-cbind(0, X)%*%coefs
+  SE<-mean((Y-yhat)^2)
+  output<-cbind(alpha, lambda, signif.pred, SE)
+  return(output)
 }
 
-A<-elastic_net2(n, beta, rho, alpha, 4)
-ggplot(data=A, aes(x=Rho, y=MSE, group=Alpha))+geom_line(aes(colour=Alpha))
+simulate.elastic.net<-function(n, p, d, rho, beta, alpha){
+  A<-diag(1, d, d)
+  for(i in 1:d){
+    for(j in 1:d){
+      if(j!=i){A[i,j]<-rho}}}
+  Cov<-diag(1, p, p)
+  for(i in 1:d){
+    for( j in 1:d){
+      Cov[i,j]<-A[i,j]}}
+  X<-mvrnorm(n, rep(0, p), Cov)
+  Y<-X%*%beta+rnorm(n)
+  mod<-elastic.net.est(n, X, Y, alpha)
+  return(mod)}
+
+MonteCarlo.elastic.net<-function(n, p, d, rho, beta, alpha, K){
+  output<-cbind(1:K, 0, 0, 0, 0)
+  for(k in 1:K){
+    mod<-simulate.elastic.net(n, p, d, rho, beta, alpha)
+    output[k,(2:5)]<-mod
+  }
+  return(output)
+}
+
+vector.MonteCarlo.En<-function(n, p, d, Rho, beta, Alpha, K){
+  results<-expand.grid(Alpha, Rho)
+  results<-cbind(results, 0, 0, 0)
+  colnames(results)<-c("Alpha", "Rho", "Lambda", "No. Variables", "MSE")
+  for(i in 1:length(results[,1])){
+    mod<-MonteCarlo.elastic.net(n, p, d, results[i,2], beta, results[i,1], K)
+    results[i, 3]<-mean(mod[,3])
+    results[i, 4]<-mean(mod[,4])
+    results[i, 5]<-mean(mod[,5])
+  }
+  return(results)
+  
+}
+
+
+
+n<-100 #number of observations
+beta<-c(1,1,2) #true values of beta
+rho<-0.99 #values of cov(X_1, X_2)
+alpha<-c(0, 0.3, 0.7, 1) 
+
+mod1<-vector.MonteCarlo.En(n, p=3, d=2, rho, beta, alpha,1)
+ggplot(mod1, aes(x=))
+
+
+
+#Now suppose n<p
+#Suppose we have a group of d predictor variables that are correlated, i.e. they all have correlation rho.
+n<-50
+p<-100
+d<-5
+rho<-0.75
+beta<-c(rep(1, d), runif(p-d, -2, 2))
+alpha<-0.9
+
+mod2<-simulate.elastic.net(n, p, d, rho, beta, alpha)  
+
+mod3<-MonteCarlo.elastic.net(n, p, d, rho, beta, alpha, K)
+
+
+
+K<-10
+Alpha<-c(0.1, 0.3, 0.9)
+Rho<-seq(0, 0.9, 0.25)
+
+mod4<-vector.MonteCarlo.En(n, p, d, Rho, beta, Alpha, K)
+
+
+
+
